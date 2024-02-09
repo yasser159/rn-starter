9826f39a0951ec1b51901a8f7bf375ac2c99452e
Yasser Hajlaoui <yasser159@gmail.com>
2024-02-08 20:20:02 -0800

    Add FlatList to ColorScreen

--- src/screens/ColorScreen.js
+++ src/screens/ColorScreen.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { View, StyleSheet, Button } from "react-native";
+import { View, StyleSheet, Button, FlatList } from "react-native";
 
 const ColorScreen = () => {
   const [colors, setcolors] = useState([]);
@@ -14,11 +14,20 @@ const ColorScreen = () => {
           setcolors([...colors, randomRgb()]);
         }}
       />
-      <View
-        style={{
-          height: 100,
-          width: 100,
-          backgroundColor: randomRgb(),
+
+      <FlatList
+        keyExtractor={(item) => item}
+        data={colors}
+        renderItem={({ item }) => {
+          return (
+            <View
+              style={{
+                height: 100,
+                width: 100,
+                backgroundColor: item,
+              }}
+            />
+          );
         }}
       />
     </View>
