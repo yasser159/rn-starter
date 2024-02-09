fb33d1462ebe9d8ed0cfb1f0e378fc9538248cd7
Yasser Hajlaoui <yasser159@gmail.com>
2024-02-08 19:56:30 -0800

    Add Counter and Color screens

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -19,6 +19,16 @@ const HomeScreen = ({ navigation }) => {
         title="Go to Image Demo"
         onPress={() => navigation.navigate("Image")}
       />
+
+      <Button
+        title="Go to Counter Demo"
+        onPress={() => navigation.navigate("Counter")}
+      />
+
+      <Button
+        title="Go to Color Demo"
+        onPress={() => navigation.navigate("Color")}
+      />
     </View>
   );
 };
